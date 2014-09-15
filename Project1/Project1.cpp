da12b5b33ba86283ff93852e2a57654faf9c8321
maschits <maschits@muohio.edu>
2014-09-15 14:19:46 -0400

    Added user inputs for dynamic matrix sizing.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  cin >> Matrix1Rows;
+  cin >> Matrix1Columns;
+  cin >> Matrix2Rows;
+  cin >> Matrix2Columns;
+
+  for (int i = 0; i < Matrix1Rows; i++) {
+    
+  }
+}
