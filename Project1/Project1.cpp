bebb1dd23e11b79b518571190837319145dd93e1
maschits <maschits@muohio.edu>
2014-09-16 11:54:46 -0400

    Prototypes added in beginning of program. Comments added to subsequent functions.

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,41 +1,14 @@
 #include <iostream>
 #include <vector>
 
+//Use the standard namespace
 using namespace std;
 
-vector< vector <int> > multiply(vector <vector <int> > Matrix1, vector <vector <int> > Matrix2) {
-  int m1rows,m1columns,m2rows,m2columns;
- cout << endl;
-  
-  m1rows = Matrix1.size();
-  m1columns = Matrix1[0].size();
-  m2rows = Matrix2.size();
-  m2columns = Matrix2[0].size();
-  vector< vector <int> > Matrix3(m1rows,vector <int>(m2columns));
-  for (int i = 0; i < m2columns; i++) {
-    for (int j = 0; j < m2rows; j++) {
-      for (int k = 0; k < m1rows; k++) {
-	Matrix3[k][i] += Matrix1[k][j]*Matrix2[j][i];
-      }
-    }
-  }
-  return Matrix3;
-}
+//Set up prototype functions 
+vector <vector <int> > multiply(vector <vector <int> > Matrix1, vector <vector <int> > Matrix2);
+void displayMatrix(vector <vector <int> > Matrix);
 
-void displayMatrix(vector <vector<int> > Matrix) {
-  int MatrixRows = Matrix.size();
-  int MatrixColumns = Matrix[0].size();
- 
-  for (int i = 0; i < MatrixRows; i++) {
-    for (int j = 0; j < MatrixColumns; j++) {
-      //Print contents of matrix cell                                          
-      cout << Matrix[i][j] << " ";
-    }
-    //Jump to next line for printing                                           
-    cout << "\n"; 
-  }
-}
-                            
+//Main function                           
 int main() {
 
   //Declare matrix-related variables
@@ -84,10 +57,59 @@ int main() {
   //Display Matrix2
   displayMatrix(Matrix2);
 
-  //Multiply the two matricies
+  //Multiply the two matrices
   Matrix3 = multiply(Matrix1,Matrix2);
   
   //Display output of multiplication
   displayMatrix(Matrix3);
   return 0;
+}
+
+//Matrix multiplication function
+vector< vector <int> > multiply(vector <vector <int> > Matrix1, vector <vector <int> > Matrix2) {
+  
+  //Declaration of function-level variables
+  int m1rows,m1columns,m2rows,m2columns;
+
+  //Skip a line
+  cout << endl;
+  
+  //Fill function-level variables
+  m1rows = Matrix1.size();
+  m1columns = Matrix1[0].size();
+  m2rows = Matrix2.size();
+  m2columns = Matrix2[0].size();
+  
+  //Declare and size the output matrix
+  vector< vector <int> > Matrix3(m1rows,vector <int>(m2columns));
+ 
+  //Loop through the two matrices to multiply them and store
+  //the result in the output matrix
+  for (int i = 0; i < m2columns; i++) {
+    for (int j = 0; j < m2rows; j++) {
+      for (int k = 0; k < m1rows; k++) {
+	Matrix3[k][i] += Matrix1[k][j]*Matrix2[j][i];
+      }
+    }
+  }
+  //Return the output matrix
+  return Matrix3;
+}
+
+//Display matrix function
+void displayMatrix(vector <vector<int> > Matrix) {
+ 
+  //Declare and fill function-level variables
+  int MatrixRows = Matrix.size();
+  int MatrixColumns = Matrix[0].size();
+ 
+  //Loop through matrix to display contents element by element
+  for (int i = 0; i < MatrixRows; i++) {
+    for (int j = 0; j < MatrixColumns; j++) {
+      //Print contents of matrix cell                                          
+      cout << Matrix[i][j] << " ";
+    }
+    //Jump to next line for printing                                           
+    cout << "\n"; 
+  }
 }
