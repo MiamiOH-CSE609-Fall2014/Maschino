3ceab5e88812b032676bfa7ae441e9cf8b8e7903
maschits <maschits@muohio.edu>
2014-09-22 09:05:03 -0400

    Modified Activity 8 c++ file. Added all necessary functionality.

--- Activities/Act8/Act8.cpp
+++ Activities/Act8/Act8.cpp
@@ -2,10 +2,37 @@
 
 using namespace std;
 
+void menu();
+
 int main() {
-  cout << "Please enter a single character to make a choice from the menu." << endl;
-  cout << "Menu:" << "\n" << "'p' - print" <<  endl;
+
+  char selection;
+  cout << "\nPlease enter a single character to make a choice from the menu." << endl;
+  menu();
+  while (selection != 'q') {
+    cin >> selection;
+    switch(selection) {
+    case 'p' : cout << "\nPrinting information for existing customer..." << endl;
+      menu();
+      break;
+    case 'a' : cout << "\nAdding information for new customer..." << endl;
+      menu();      
+      break;
+    case 'e' : cout << "\nEditing information for existing customer..." << endl;
+      menu();
+      break;
+    case 'd' : cout << "\nDeleting information for existing customer..." << endl;
+      menu();      
+      break;
+    case 'q' : cout << "\nThank you for using the program. Goodbye." << endl;
+      break;
+    default : cout << "\nInvalid input. Please enter your selection again." << endl;
+      menu();
+    }
+  }
+}
+  void menu() {
+  cout << "\nMenu:" << "\n" << "'p' - print" <<  endl;
   cout << "'a' - add" << "\n'e' - edit" << endl;
-  cout << "'d' - deleted" << "\n'q' - quit" << endl;
-  return 0;
+  cout << "'d' - deleted" << "\n'q' - quit\n" << endl;
 }
