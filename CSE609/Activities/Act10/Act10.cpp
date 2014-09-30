71f8839e51fbb7e2583c953165172863dd6b8b69
maschits <maschits@muohio.edu>
2014-09-30 12:26:22 -0400

    Added use of try/catch blocks.

--- CSE609/Activities/Act10/Act10.cpp
+++ CSE609/Activities/Act10/Act10.cpp
@@ -3,26 +3,58 @@
 
 using namespace std;
 
-const int max_values = 100;
-const int columns = 200;
-const int rows = 100;
+const int max_values = 1000;
+const int max_columns = 200;
+const int max_rows = 100;
 
-void getInput(int r, int c);
+int rows;
+int columns;
+
+void getInput(int &r, int &c);
 
 int main() {
   
   getInput(rows,columns); 
- 
-
 }
 
-void getInput(int r, int c) {
-  cout << "Enter the rows: ";
-  cin >> r;
-  cout << "Enter the columns: ";
-  cin >> c;
+void getInput(int &r, int &c) {
+   
+  try {
+   
+    cout << "Enter the rows: ";
+    cin >> r;
+    cout << "Enter the columns: ";
+    cin >> c;
 
-  if (r > rows || c > columns) {
+     if (r > max_rows || c > max_columns) {
+       throw out_of_range("Matrix rows or columns are greater than the maximum allowed.");
+    }
+     if ((r * c) > max_values) {
+       throw range_error("The total size of the matrix exceeds the total allowed size.");
+     }
+  }
+  catch (out_of_range err) {
+    cout << err.what() << "\nTry again? Enter y or n" << endl;
+    char c;
+    cin >> c;
+    if (c == 'y') {
+      getInput(rows,columns);
+    }
+  }
+  catch (range_error err) {
+    cout << err.what() << "\nTry again? Enter y or n" << endl;
+    char c;
+    cin >> c;
+    if (c == 'y') {
+      getInput(rows,columns);
+    }
+  }
+  /*
+  if (r > max_rows || c > max_columns) {
     throw out_of_range("The input is too large");
   }
+  else {
+    rows = r;
+    columns = c;
+    }*/
 }
