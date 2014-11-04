124df5d933cd704b45eccbfc25e87b721295bf19
maschits <maschits@muohio.edu>
2014-11-04 12:17:58 -0500

    Finalized activity 19 files.

--- CSE609/Activities/Act19/Activity19.cpp
+++ CSE609/Activities/Act19/Activity19.cpp
@@ -18,9 +18,21 @@ int main() {
   ifstream myfile ("students.csv");
 
       while (myfile.good() ) {
-	getline(myfile,value[0],',');
-	getline(myfile,value[1],',');
-	getline(myfile,value[2],'\n');
+	string temp;
+	getline(myfile,temp,'\n');
+
+	//cout << temp << endl;
+
+	int s = temp.find(",");	
+	value[0] = temp.substr(0,s);
+	//cout << value[0] << endl;
+
+	s = temp.substr(s+1,temp.length()-1).find(",");
+	value[1] = temp.substr(value[0].length()+1,s);
+	//cout << value[1] << endl;
+
+	value[2] = temp.substr(value[1].length()+value[0].length()+2,temp.length()-1);
+	//cout << value[2] << endl;
 	info[value[0]] = make_pair(value[1],value[2]);
       }
       myfile.close(); 
@@ -28,8 +40,21 @@ int main() {
       string input;
       while (input!="quit") {
 	cin >> input;
-	//	p = info.find(input);
-	cout << "Name: " << info.begin()->first << endl;
+	try {
+	  if (input!="quit") {
+	    if (info.count(input) < 1) {
+	      throw invalid_argument("Please enter a valid name.");
+	    }
+	    else {
+	      cout << "Name: " << info.find(input)->first << endl;
+	      cout << "Major: " << get<0>(info.find(input)->second) << endl;
+	      cout << "GPA: " << get<1>(info.find(input)->second) << endl;
+	    }
+	  }
+	}
+	catch (invalid_argument e) {
+	  cout << e.what() << endl;
+	}
       }
 
   return 0;
