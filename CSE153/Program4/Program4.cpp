81de80d26919942f7099a9928a1f07eeb133dda6
Tyler Maschino <Maschits@miamioh.edu>
2015-03-19 13:54:05 -0400

    Added programs 4 and 5

--- CSE153/Program4/Program4.cpp
+++ CSE153/Program4/Program4.cpp
@@ -1,9 +1,22 @@
+/*Program 4: Develop a program that takes in user input for widths of coins.
+* The input is analyzed and, using decisions, computed the total sum of all
+* coins entered. In the event that a coin is not a valid width, the system
+* does not consider it in the final total. A '0' is used to tell the system
+* to stop looping and print out the running total.
+* CSE 153
+* Tyler Maschino
+*/
+
+// Include external libraries
 #include <iostream>
 
+// Use the 'standard' namespace
 using namespace std;
 
+// Main function of program
 int main() {
 	
+	// Declare necessary variables
 	double nickelMin = 0.8183;
 	double nickelMax = 0.8517;
 	double dimeMin = 0.6909;
@@ -14,13 +27,17 @@ int main() {
 	double balance = 0.0;
 	cout.precision(3);
 	
+	// List acceptable width ranges
 	cout << "Acceptable width of a nickel is from " << nickelMin << " to " << nickelMax << endl;
 	cout << "Acceptable width of a dime is from " << dimeMin << " to " << dimeMax << endl;
 	cout << "Acceptable width of a quarter is from " << quarterMin << " to " << quarterMax << endl;
 	cout << endl;
 	
+	// Get user input for coin widths
 	cin >> input;
 	while (input != 0.0) {
+
+		// Add balance of a nickel, dime, or quarter, depending on entered widths
 		if ((input >= nickelMin) && (input <= nickelMax)) {
 			balance += 0.05;
 		}
@@ -37,7 +54,12 @@ int main() {
 		cin >> input;
 	}
 	
+	// Display the value of the coins entered
 	cout << "\nvalue of coins entered: $" << balance << endl;
+
+	// Pause the program before exiting to show inputs/outputs
+	cin.get();
+	cin.get();
 	
 	return 0;
 }
--- CSE153/Program5/Program5.cpp
+++ CSE153/Program5/Program5.cpp
@@ -1,9 +1,22 @@
+/*Program 5: Develop a program that takes in user input for widths of coins.
+* The input is analyzed and, using decisions, computed the total sum of all
+* coins entered. In the event that a coin is not a valid width, the system
+* does not consider it in the final total. A '0' is used to tell the system
+* to stop looping and print out the running total.
+* CSE 153
+* Tyler Maschino
+*/
+
+// Include external libraries
 #include <iostream>
 
+// Use the 'standard' namespace
 using namespace std;
 
+// Main function of program
 int main() {
 	
+	// Declare necessary variables
 	double nickelWidthMin = 0.8183;
 	double nickelWidthMax = 0.8517;
 	double dimeWidthMin = 0.6909;
@@ -25,25 +38,35 @@ int main() {
 	int tests;
 	cout.precision(3);
 	
+	// List acceptable width and weight ranges
 	cout << "Acceptable width of a nickel is from " << nickelWidthMin << " to " << nickelWidthMax << endl;
 	cout << "Acceptable width of a dime is from " << dimeWidthMin << " to " << dimeWidthMax << endl;
 	cout << "Acceptable width of a quarter is from " << quarterWidthMin << " to " << quarterWidthMax << endl;
-	cout << "Acceptable width of a nickel is from " << nickelWeightMin << " to " << nickelWeightMax << endl;
-	cout << "Acceptable width of a dime is from " << dimeWeightMin << " to " << dimeWeightMax << endl;
-	cout << "Acceptable width of a quarter is from " << quarterWeightMin << " to " << quarterWeightMax << endl;
+	cout << "Acceptable weight of a nickel is from " << nickelWeightMin << " to " << nickelWeightMax << endl;
+	cout << "Acceptable weight of a dime is from " << dimeWeightMin << " to " << dimeWeightMax << endl;
+	cout << "Acceptable weight of a quarter is from " << quarterWeightMin << " to " << quarterWeightMax << endl;
 	cout << endl;
 	
+
+	// Get user input for number of tests
 	cout << "How many tests do you want to do? ";
 	cin >> tests;
 	
+	// Loop through each test
 	for (int i = 0; i < tests; i++) {
+
+		// Reset the balance
 		balance = 0;
+
+		// Print out the test number
 		cout << "Test " << i+1 << endl;
 		cout << endl;
 		
+		// Get coin width and weight
 		cin >> width;
 		cin >> weight;
 		
+		// Add balance of a nickel, dime, or quarter, depending on entered widths
 		while ((width != 0.0) && (weight != 0.0)) {
 			if ((width >= nickelWidthMin) && (width <= nickelWidthMax) && (weight >= nickelWeightMin) && (weight <= nickelWeightMax))  {
 				balance += 0.05;
@@ -62,10 +85,13 @@ int main() {
 			cin >> weight;
 		}
 	
+		// Display the value of the coins entered
 		cout << "\nvalue of coins entered: $" << balance << endl;
 	}
 	
-	
+	// Pause the program before exiting to show inputs/outputs
+	cin.get();
+	cin.get();
 	
 	return 0;
 }
